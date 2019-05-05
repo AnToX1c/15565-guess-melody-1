94964e9a7fc7e7b44e9f4fa4a6d1d77185135923
Anton Lapshin <anton.lapshin.s@gmail.com>
2019-05-05 09:21:31 +0300

    Format files

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import WelcomeScreen from '../../components/welcome-screen/welcome-screen.jsx';
+import React from "react";
+import WelcomeScreen from "../../components/welcome-screen/welcome-screen.jsx";
 
 const App = (props) => {
   const {gameTime, errorCount} = props;
 
-  return <WelcomeScreen
-    time = {gameTime}
-    errorCount = {errorCount}
-  />;
+  return <WelcomeScreen time={gameTime} errorCount={errorCount} />;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/app.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
 
 const init = () => {
   const settings = {
     gameTime: 5,
-    errorCount: 3,
+    errorCount: 3
   };
 
   ReactDOM.render(
-      <App
-        gameTime = {settings.gameTime}
-        errorCount = {settings.errorCount}
-      />,
+      <App gameTime={settings.gameTime} errorCount={settings.errorCount} />,
       document.querySelector(`.main`)
   );
 };
