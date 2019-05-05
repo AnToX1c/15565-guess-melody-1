a929c6e583e6f67a5387ec94e68e59b71019b5e9
Anton Lapshin <anton.lapshin.s@gmail.com>
2019-05-05 23:16:48 +0300

    Install 'prop-types'

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import WelcomeScreen from "../../components/welcome-screen/welcome-screen.jsx";
+import PropTypes from "prop-types";
 
 const App = ({gameTime, errorCount}) => (
   <WelcomeScreen time={gameTime} errorCount={errorCount} />
 );
+
+App.propTypes = {
+  gameTime: PropTypes.number.isRequired,
+  errorCount: PropTypes.number.isRequired
+};
 
 export default App;
